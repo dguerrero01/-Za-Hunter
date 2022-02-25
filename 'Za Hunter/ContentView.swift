bdd3c63273d86af549b425f9efc89fd8274f1e08
Daisy Guerrero <dguerrero1@bsd220.org>
2022-02-25 10:32:56 -0600

    Initial Commit

---
+++ 'Za Hunter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  'Za Hunter
+//
+//  Created by Student on 2/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
